57a2588104333f910133a1cbcae2cd6b99f9e822
Max-Bevelander-Miro <max.b@miro.com>
2026-03-11 21:47:47 +0100

    add casper as user, and update passwords

--- src/main/resources/db/migration/V1__create_users_table.sql
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -7,5 +7,6 @@ CREATE TABLE users (
 );
 
 INSERT INTO users (username, password, role) VALUES
-    ('max', '$2a$10$ens3HaIDUUdSsvKObvq9AexcHWL.Kx62frHLmyDRMxyjYWuzm9/dq', 'ADMIN'),
-    ('emily', '$2a$10$ens3HaIDUUdSsvKObvq9AexcHWL.Kx62frHLmyDRMxyjYWuzm9/dq', 'ADMIN');
+    ('max', '$2a$10$d9.Cz8wmPGNbg.61nUfrzeM9u.1wbF/c7yWaYh.1YEBUyDhpYN3j6', 'ADMIN'),
+    ('emily', '$2a$10$9QI6KsLA9nesYsS2mRWDxu83Kvz4jZI7LfbyteEbc.PHct0IWeEt.', 'ADMIN'),
+    ('casper', '$2a$10$jJX3POeJka9ki1hlvcOGtulrTMQkjZU8jtoKsAlJCRbqLvNEYd8qi', 'ADMIN');
